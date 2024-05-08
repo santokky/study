56742a2c86d52326fe791aebb675178362fd8ccb
santokky <csq119@naver.com>
2024-05-08 09:10:50 +0900

    Unity project for learning

---
+++ Fast Campus/Assets/ControllerCharacter/ControllerCharacter.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerCharacter : MonoBehaviour
+{
+    #region Variables
+    public float speed = 5f;
+    public float jumpHeight = 2f;
+    public float dashDistance = 6f;
+
+    public float gravity = -9.81f;
+    public Vector3 drags = new Vector3(8, 8, 8);
+    
+    private CharacterController characterController;
+
+    private Vector3 inputDirection = Vector3.zero;
+
+    private bool isGrounded = false;
+    public LayerMask groundLayerMask;
+    public float groundCheckDistance = 0.3f;
+
+    private Vector3 calcVelocity;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = characterController.isGrounded;
+
+        if (isGrounded && calcVelocity.y < 0) {
+            calcVelocity.y = 0;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.Move(move * Time.deltaTime * speed);
+
+        if (move != Vector3.zero) {
+            transform.forward = move;
+        }
+        
+        //process jump input
+        if (isGrounded)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+                calcVelocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward,
+                dashDistance * new Vector3(3f, 0, 3f));
+
+            calcVelocity += dashVelocity;
+
+         }
+
+        calcVelocity.y += gravity * Time.deltaTime;
+
+        calcVelocity.x /= 1 + drags.x * Time.deltaTime;
+        calcVelocity.y /= 1 + drags.y * Time.deltaTime;
+        calcVelocity.z /= 1 + drags.z * Time.deltaTime;
+
+        characterController.Move(calcVelocity * Time.deltaTime);
+    }
+}
---
+++ Fast Campus/Assets/RigidBodyCharacter/RigidBodyCharacter.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBodyCharacter : MonoBehaviour
+{
+    #region Variables
+    public float speed = 5f;
+    public float jumpHeight = 2f;
+    public float dashDistance = 6f;
+
+    private Rigidbody rb;
+
+    private Vector3 inputDirection = Vector3.zero;
+
+    private bool isGrounded = false;
+    public LayerMask groundLayerMask;
+    public float groundCheckDistance = 0.3f;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckGroundStatus();
+
+        inputDirection = Vector3.zero;
+        inputDirection.x = Input.GetAxis("Horizontal");
+        inputDirection.z = Input.GetAxis("Vertical");
+        if (inputDirection != Vector3.zero) {
+            transform.forward = inputDirection;
+        }
+
+        //process jump input
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+            Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(
+                jumpHeight * -2f * Physics.gravity.y);
+            GetComponent<Rigidbody>().AddForce(jumpVelocity, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward,
+                dashDistance * new Vector3(3f, 0, 3f));
+
+            GetComponent<Rigidbody>().AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
+            + inputDirection * speed * Time.fixedDeltaTime);
+    }
+
+    #region Helper Methods
+    void CheckGroundStatus() {
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down,
+            groundCheckDistance, groundLayerMask))
+        {
+            isGrounded = true;
+            Debug.Log("¶¥ ¹âÀ½");
+        }
+        else {
+            isGrounded = false;
+            Debug.Log("¶¥ ¸ø¹âÀ½");
+        }
+    }
+
+    #endregion
+}
